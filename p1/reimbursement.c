7ba8a6f75c33dce4af6cc19454f4c88c5af3f941
clwhe <clwhe@users.noreply.github.com>
2017-07-22 12:20:02 -0700

    Create reimbursement.c

---
+++ p1/reimbursement.c
@@ -0,0 +1,27 @@
+/* clwhe */
+
+#include <stdio.h>
+
+#define DOLLAR_RATE 0.35
+
+int main()
+{
+  double begin_miles;
+  double end_miles;
+  double traveled_miles;
+  double reimbursement;
+  
+  printf("MILEAGE REIMBURSEMENT CALCULATOR\n");
+  printf("Enter beginning odometer reading=> ");
+  scanf("%lf", &begin_miles);
+  printf("Enter ending odometer reading=> ");
+  scanf("%lf", &end_miles);
+  
+  traveled_miles = end_miles - begin_miles;
+  reimbursement = traveled_miles * DOLLAR_RATE;
+  
+  printf("You traveled %.1f miles. At $.35 per mile,\n", traveled_miles);
+  printf("your reimbursement is $%.2f.\n", reimbursement);
+  
+  return 0;
+}
