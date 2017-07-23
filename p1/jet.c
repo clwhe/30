d9fc8488312eedca54eb34a6a13f8f8d91e35af1
clwhe <clwhe@users.noreply.github.com>
2017-07-22 17:52:55 -0700

    Create jet.c

---
+++ p1/jet.c
@@ -0,0 +1,26 @@
+/* clwhe */
+
+#include <stdio.h>
+
+int main()
+{
+  int kph;
+  double mps;
+  int dist;
+  double accel;
+  double time;
+  
+  printf("Please enter the takeoff speed of the jet in km/hr > ");
+  scanf("%d", &kph);
+  printf("Please enter the catapult distance in meters > ");
+  scanf("%d", &dist);
+  
+  mps = (double)kph * 1000 / 3600;  /* convert km/hr to m/s */
+  time = 2 * dist / mps;
+  accel = mps / time;
+  
+  printf("The jet would accelerate at %.2f m/s^2 for %.2f seconds.\n",
+         accel, time);
+         
+  return 0;
+}
