b5adef33a8e7ff14d9e8deac29eac7f42066ea0c
clwhe <clwhe@users.noreply.github.com>
2017-07-23 12:26:49 -0700

    Create pop.c

---
+++ p2/pop.c
@@ -0,0 +1,36 @@
+/* clwhe */
+
+#include <stdio.h>
+
+#define INIT_POP 52.966   /* population in 1990 */
+#define POP_GROWTH 2.184  /* growth factor */
+#define INIT_YEAR 1990    /* zeroth year */
+
+double population(int year);
+
+int main()
+{
+  int year;
+  double predicted_pop;
+  FILE *outp;
+  
+  outp = fopen("pop.txt", "w");
+  
+  printf("Enter a year after 1990> ");
+  scanf("%d", &year);
+  
+  predicted_pop = population(year);
+  
+  fprintf(outp, "Predicted Gotham City population for");
+  fprintf(outp, " %d (in thousands): %.3f\n", year, predicted_pop);
+  
+  fclose(outp);
+  
+  return 0;
+}  /* main() */
+
+
+double population(int year)
+{
+  return INIT_POP + POP_GROWTH * (year - INIT_YEAR);
+}  /* population() - predicts population for input year */
