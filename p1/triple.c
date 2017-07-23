1903d5c18ef33afd1638e6fcc869781c1b3e0340
clwhe <clwhe@users.noreply.github.com>
2017-07-22 17:41:04 -0700

    Create triple.c

---
+++ p1/triple.c
@@ -0,0 +1,24 @@
+/* clwhe */
+
+#include <stdio.h>
+
+int main()
+{
+  int m;
+  int n;
+  int side1;
+  int side2;
+  int hypot;
+  
+  printf("Please enter two integers with the first larger than the second > ");
+  scanf("%d %d", &m, &n);
+  
+  side1 = m * m - n * n;
+  side2 = 2 * m * n;
+  hypot = m * m + n * n;
+  
+  printf("The Pythagorean triple of %d and %d is %d, %d, and %d.\n",
+         m, n, side1, side2, hypot);
+         
+  return 0;
+}
