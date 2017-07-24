cdc26dac70ffc3f26f2a2e6ed9ddc4b515361b46
clwhe <clwhe@users.noreply.github.com>
2017-07-24 13:52:15 -0700

    Update house.c

--- p2/house.c
+++ p2/house.c
@@ -2,16 +2,16 @@
 
 #include <stdio.h>
 
-void instruct(void);
+void instruct_user(void);
 
-int main()
+int main(void)
 {
   int init_cost;
   int fuel_cost;
   double tax_rate;
   double total_cost;
   
-  instruct();
+  instruct_user();
   
   printf("Please enter the initial cost, fuel cost, and tax rate: ");
   scanf("%d %d %lf", &init_cost, &fuel_cost, &tax_rate);
@@ -22,13 +22,13 @@ int main()
   printf("The total cost is $%.2f.\n", total_cost);
   
   return 0;
-}  /* main() */
+}
 
 
-void instruct(void)
+void instruct_user(void)
 {
   printf("This program determines the total cost of owning a home");
   printf(" for five years.\nThe user will enter initial cost in");
   printf(" whole dollars, annual fuel costs in\nwhole dollars,");
   printf(" and the annual tax rate as a real number.\n\n");
-}  /* instruct() - displays instructions for user */
+}
