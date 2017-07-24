da044b6a8c5c97b44f370a8309c92bf458f3d445
clwhe <clwhe@users.noreply.github.com>
2017-07-24 14:26:01 -0700

    Update emissions.c

--- p3/emissions.c
+++ p3/emissions.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
   int pollutant;
   int odometer;
@@ -18,49 +18,40 @@ int main()
   printf("Enter odometer reading>> ");
   scanf("%d", &odometer);
   
-  if (pollutant == 1)
-  {
+  if (pollutant == 1) {
     if (odometer <= 50000)
       limit = 3.4;
-    else  /* odometer > 50000 */
+    else
       limit = 4.2;
   }
-  
-  else if (pollutant == 2)
-  {
+  else if (pollutant == 2) {
     if (odometer <= 50000)
       limit = 0.31;
-    else  /* odometer > 50000 */
+    else
       limit = 0.39;
   }
-  
-  else if (pollutant == 3)
-  {
+  else if (pollutant == 3) {
     if (odometer <= 50000)
       limit = 0.4;
-    else  /* odometer > 50000 */
+    else
       limit = 0.5;
   }
-  
-  else if (pollutant == 4)
-  {
+  else if (pollutant == 4) {
     if (odometer <= 50000)
       limit = 0.25;
-    else  /* odometer > 50000 */
+    else
       limit = 0.31;
   }
-  
-  else  /* invalid pollutant number entered */
-  {
+  else {
     printf("Invalid pollutant number.\n");
     return 1;
   }
   
   if (grams_mile > limit)
     printf("Emissions exceed permitted level of %.2f grams/mile.\n", limit);
-  else  /* emissions limit not exceeded */
+  else
     printf("Emissions do not exceed permitted level of %.2f grams/mile.\n",
            limit);
   
   return 0;
-}  /* main() */
+}
