f2af735dc18cbaf86a4bba8c499e58f65cbaa534
clwhe <clwhe@users.noreply.github.com>
2017-07-24 13:48:02 -0700

    Update can.c

--- p2/can.c
+++ p2/can.c
@@ -3,45 +3,44 @@
 #include <stdio.h>
 #include <math.h>
 
-void instruct(void);
-double surface_area(double radius, double height);
+void instruct_user(void);
+double open_cyl_area(double radius, double height);
 
-int main()
+int main(void)
 {
   double radius;
   double height;
   double cost_per_sq_cm;
-  double surf_area;  /* surface area of one can */
+  double can_area;
   double cost_per_can;
   double total_cost;
   int quantity;
-  int row;    /* row of input file */
+  int row;    /* input file line number */
   FILE *inp;  /* pointer to input file */
   
-  instruct();
+  instruct_user();
   
   inp = fopen("cans.txt", "r");
   
-  for (row = 1; row <= 3; row++)
-  {
+  for (row = 1; row <= 3; row++) {
     fscanf(inp, "%lf %lf", &radius, &height);
     fscanf(inp, "%lf %d", &cost_per_sq_cm, &quantity);
     
-    surf_area = surface_area(radius, height);
-    cost_per_can = surf_area * cost_per_sq_cm;
+    can_area = open_cyl_area(radius, height);
+    cost_per_can = can_area * cost_per_sq_cm;
     total_cost = quantity * cost_per_can;
     
-    printf("Each can #%d has surface area %8.3f,", row, surf_area);
+    printf("Each can #%d has surface area %8.3f,", row, can_area);
     printf(" and the run will cost $%.2f.\n", total_cost);
-  }  /* process file line by line for 3 can types */
+  }
   
   fclose(inp);
   
   return 0;
-}  /* main() */
+}
 
 
-void instruct(void)
+void instruct_user(void)
 {
   printf("This program calculates the cost of materials for three");
   printf(" open-top cylindrical\ncontainers. It reads the radius,");
@@ -49,16 +48,16 @@ void instruct(void)
   printf(" each container from the file cans.txt. Each line in");
   printf(" cans.txt has\nthe format: radius height ");
   printf("cost_per_sq_cm quantity.\n\n");
-}  /* instruct() - displays instructions for user */
+}
 
 
-double surface_area(double radius, double height)
+double open_cyl_area(double radius, double height)
 {
   double bottom_area;
   double side_area;
   
   bottom_area = M_PI * radius * radius;
   side_area = 2 * M_PI * radius * height;
-  
+  /* open-top cylinder area */
   return (bottom_area + side_area);
-}  /* surface_area() - calculates open-top cylinder surface area */
+}
