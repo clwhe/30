8ef2e6c1f5f90794b23e6c0412b41bb3e5b3116a
clwhe <clwhe@users.noreply.github.com>
2017-07-27 00:08:10 -0700

    Update reimbursement.c
    Added user input checks

--- p1/reimbursement.c
+++ p1/reimbursement.c
@@ -4,6 +4,8 @@
 
 #define DOLLAR_RATE 0.35
 
+void display_instructions(double *begin_miles, double *end_miles);
+
 int main(void)
 {
   double begin_miles;
@@ -11,17 +13,28 @@ int main(void)
   double traveled_miles;
   double reimbursement;
   
-  printf("MILEAGE REIMBURSEMENT CALCULATOR\n");
-  printf("Enter beginning odometer reading=> ");
-  scanf("%lf", &begin_miles);
-  printf("Enter ending odometer reading=> ");
-  scanf("%lf", &end_miles);
-  
-  traveled_miles = end_miles - begin_miles;
-  reimbursement = traveled_miles * DOLLAR_RATE;
+  do {
+    display_instructions(&begin_miles, &end_miles);
+    traveled_miles = end_miles - begin_miles;
+    
+    if (traveled_miles >= 0)
+      reimbursement = traveled_miles * DOLLAR_RATE;
+    else  /* traveled_miles < 0 */
+      printf("Invalid odometer reading input!\n");
+  } while (traveled_miles < 0);
   
   printf("You traveled %.1f miles. At $.35 per mile,\n", traveled_miles);
   printf("your reimbursement is $%.2f.\n", reimbursement);
   
   return 0;
+}
+
+
+void display_instructions(double *begin_miles, double *end_miles);
+{
+  printf("MILEAGE REIMBURSEMENT CALCULATOR\n");
+  printf("Enter beginning odometer reading=> ");
+  scanf("%lf", begin_miles);
+  printf("Enter ending odometer reading=> ");
+  scanf("%lf", end_miles);
 }
